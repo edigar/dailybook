d8b8825e19ffeef34722872e0948fd638fb1eece
Edigar Herculano <edigarhdev@gmail.com>
2023-11-15 15:16:07 -0300

    refactor: component header to navbar

--- resources/views/edit_note.blade.php
+++ resources/views/edit_note.blade.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <x-header />
+    <x-navbar />
     <div class="container">
         <div class="row mt-4 mb-5 justify-content-center">
             <div class="col-xs-10 col-sm-10">
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <x-header />
+    <x-navbar />
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
